777d3d35b46525cc82c9f916d1bd0aec0354d958
YongHo, Jeong <yonghojeong57@gmail.com>
2020-01-29 22:29:53 +0900

    fixed a plot style
    variable inb->iMj
    png->pdf
    extence the circle size
    modified plot title

--- rpv_macros/src/plot_piechart.cxx
+++ rpv_macros/src/plot_piechart.cxx
@@ -17,6 +17,7 @@
 #include "TPieSlice.h"
 #include "TDirectory.h"
 #include "TString.h"
+#include "TLatex.h"
 
 using namespace std;
 
@@ -26,10 +27,17 @@ void set_piechart(TPie *p_opt){
 	p_opt->GetSlice(2)->SetTitle("W+jets");
 	p_opt->GetSlice(3)->SetTitle("Other");
 	p_opt->SetTextSize(0.028);
-	p_opt->SetRadius(.30);
-	p_opt->SetLabelsOffset(.015);
+	p_opt->SetRadius(.40);
+	p_opt->SetLabelsOffset(-.22);
 	p_opt->SetLabelFormat("#splitline{%val (%perc)}{%txt}");
-	//p_opt->GetSlice(0)->SetTextSize(0.07);
+}
+
+void set_latex(TLatex lt){
+	lt.SetTextAlign(12);
+	lt.SetNDC();
+	lt.SetTextFont(132);
+	lt.SetTextAngle(0);
+	lt.SetTextSize(0.165);
 }
 
 int main()
@@ -40,44 +48,80 @@ int main()
 
 	TFile* infile  = TFile::Open(inputdir+"output_nominal_newnt.root", "READ");
 
-	float qcd[34][3], ttbar[34][3], wjets[34][3], other[34][3], sig1600[34][3];
+	float qcd[34][3], ttbar[34][3], wjets[34][3], other[34][3];//, sig1600[34][3];
 
 	for(int ibin=0; ibin<34; ibin++){
-		for(int inb=0; inb<3; inb++){
-			qcd[ibin][inb] = 0;
-			ttbar[ibin][inb] = 0;
-			wjets[ibin][inb] = 0;
-			other[ibin][inb] = 0;
-			sig1600[ibin][inb] = 0;
+		for(int iMj=0; iMj<3; iMj++){
+			qcd[ibin][iMj] = 0;
+			ttbar[ibin][iMj] = 0;
+			wjets[ibin][iMj] = 0;
+			other[ibin][iMj] = 0;
 		}
 	}
 
 	for(int ibin=22; ibin<34; ibin++){
-		for(int inb=0; inb<3; inb++){
-			qcd[ibin][inb]= static_cast<TH1F*>(infile->Get(Form("bin%i/qcd", ibin)))->GetBinContent(inb+1);
-			ttbar[ibin][inb]= static_cast<TH1F*>(infile->Get(Form("bin%i/ttbar", ibin)))->GetBinContent(inb+1);
-			wjets[ibin][inb]= static_cast<TH1F*>(infile->Get(Form("bin%i/wjets", ibin)))->GetBinContent(inb+1);
-			wjets[ibin][inb]=1.53*wjets[ibin][inb];
-			other[ibin][inb]= static_cast<TH1F*>(infile->Get(Form("bin%i/other", ibin)))->GetBinContent(inb+1);
-			sig1600[ibin][inb]= static_cast<TH1F*>(infile->Get(Form("bin%i/signal_M1600", ibin)))->GetBinContent(inb+1);
+		for(int iMj=0; iMj<3; iMj++){
+			qcd[ibin][iMj]= static_cast<TH1F*>(infile->Get(Form("bin%i/qcd", ibin)))->GetBinContent(iMj+1);
+			ttbar[ibin][iMj]= static_cast<TH1F*>(infile->Get(Form("bin%i/ttbar", ibin)))->GetBinContent(iMj+1);
+			wjets[ibin][iMj]= static_cast<TH1F*>(infile->Get(Form("bin%i/wjets", ibin)))->GetBinContent(iMj+1);
+			wjets[ibin][iMj]=1.53*wjets[ibin][iMj];
+			other[ibin][iMj]= static_cast<TH1F*>(infile->Get(Form("bin%i/other", ibin)))->GetBinContent(iMj+1);
 		}
 	}
 
 	TCanvas *cpie[12][3];
 	TPie *pie4[12][3];
-	TString GeV[3]={"=500GeV","=500~1000GeV",">1000GeV"};
-	//Float_t vals[] = {201.4,447.3,526.5,174.1,0.5};
+
+	TString title_1[36] = {
+		"",//0
+		"",
+		"",
+		"",
+		"",
+		"",//5
+		"",
+		"",
+		"",
+		"",
+		"",//10
+		"",
+		"",
+		"",
+		"",
+		"",//15
+		"",
+		"",
+		"",
+		"",
+		"",//20
+		"",
+		"1-lepton, HT>1200 GeV, 4#leq N_{jets}#leq5",//22
+		"1-lepton, HT>1200 GeV, 6#leq N_{jets}#leq7",
+		"1-lepton, HT>1200 GeV, N_{jets}#leq8", 
+		"1-lepton, HT>1200 GeV, 4#leq N_{jets}#leq5",
+		"1-lepton, HT>1200 GeV, 6#leq N_{jets}#leq7",
+		"1-lepton, HT>1200 GeV, N_{jets}#leq8",
+		"1-lepton, HT>1200 GeV, 4#leq N_{jets}#leq5",
+		"1-lepton, HT>1200 GeV, 6#leq N_{jets}#leq7",//29
+		"1-lepton, HT>1200 GeV, N_{jets}#leq8",
+		"1-lepton, HT>1200 GeV, 4#leq N_{jets}#leq5",
+		"1-lepton, HT>1200 GeV, 6#leq N_{jets}#leq7",       
+		"1-lepton, HT>1200 GeV, N_{jets}#leq8"//33
+	};
+
+	TString Mj_txt[3]={", 500<M_{j}<800",", 800<M_{j}<1100",", 1100<M_{j}"};
+
 	for(int ibin=22; ibin<34; ibin++){
-		for (int inb=0; inb<3; inb++){
-			Float_t vals[] = {qcd[ibin][inb],ttbar[ibin][inb],wjets[ibin][inb],other[ibin][inb]};
+		for (int iMj=0; iMj<3; iMj++){
+			Float_t vals[] = {qcd[ibin][iMj],ttbar[ibin][iMj],wjets[ibin][iMj],other[ibin][iMj]};
 			Int_t colors[] = {kYellow-7,kAzure+7,kGreen+2,kGray+1};
 			Int_t nvals = sizeof(vals)/sizeof(vals[0]);
-			cpie[ibin][inb] = new TCanvas(Form("cpie_%i_%i",ibin,inb),Form("TPie test_%i_%i",ibin,inb),700,700);
-			pie4[ibin][inb] = new TPie(Form("pie4_%i_%i", ibin,inb),Form("MC_piechart_bin=%i_MJ"+GeV[inb],ibin),nvals,vals,colors);
-			set_piechart(pie4[ibin][inb]);
-			if(!(ibin==31 && inb==2)){
-				pie4[ibin][inb]->Draw("nol <");
-				cpie[ibin][inb]->SaveAs(outputdir+Form("MC_piechart_bin%i_MJ"+GeV[inb]+".pdf",ibin));
+			cpie[ibin][iMj] = new TCanvas(Form("cpie_%i_%i",ibin,iMj),Form("TPie test_%i_%i",ibin,iMj),700,700);
+			pie4[ibin][iMj] = new TPie(Form("pie4_%i_%i", ibin,iMj),Form(title_1[ibin]+Mj_txt[iMj]),nvals,vals,colors);
+			set_piechart(pie4[ibin][iMj]);
+			if(!(ibin==31 && iMj==2)){
+				pie4[ibin][iMj]->Draw("nol <");
+				cpie[ibin][iMj]->SaveAs(outputdir+Form("piechart_ibin_%i_Mj_%i.pdf",ibin,iMj));
 			}
 		}
 	}
